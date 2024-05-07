df0386b01bd55b6593429017e83157bbdce90be3
mynjstr <jestertiedra@gmail.com>
2024-05-07 23:53:50 +0800

    created pseudocode

---
+++ simple_app_calculator.py
@@ -0,0 +1,7 @@
+# Define a addition function for two numbers
+# Define a subtraction function for two numbers
+# Define a multiplication function for two numbers
+# Define a divison function for two numbers
+# Construct a dictionary for operations
+# Define a function to execute the calculator
+# Try to execute the operation
