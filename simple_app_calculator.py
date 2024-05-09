f79ba7fcd65a0260b35583d279aff00d0eccc542
mynjstr <jestertiedra@gmail.com>
2024-05-09 12:25:47 +0800

    Import necessary customtkinter,tkinter and time libraries

--- simple_app_calculator.py
+++ simple_app_calculator.py
@@ -1,4 +1,9 @@
 #Import necessary tkinter libraries
+import customtkinter
+import customtkinter as ctk
+from tkinter import *
+from tkinter import messagebox
+import time
 #Set GUI theme and window size
 #Define functions for button clicks, clearing the entry, calculating the result, and asking to continue
 #Create result box (entry)
