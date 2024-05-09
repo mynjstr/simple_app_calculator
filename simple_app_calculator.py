1c73c527c9227b7ffed8f550aa5fc024ee4861f3
mynjstr <jestertiedra@gmail.com>
2024-05-09 12:27:36 +0800

    Set GUI theme and window size

--- simple_app_calculator.py
+++ simple_app_calculator.py
@@ -5,6 +5,12 @@ from tkinter import *
 from tkinter import messagebox
 import time
 #Set GUI theme and window size
+customtkinter.set_appearance_mode("light")
+customtkinter.set_default_color_theme("green")
+
+app = customtkinter.CTk()
+app.title("Simple Calculator App")
+app.geometry("300x300")
 #Define functions for button clicks, clearing the entry, calculating the result, and asking to continue
 #Create result box (entry)
 #Create buttons for numbers and operators
